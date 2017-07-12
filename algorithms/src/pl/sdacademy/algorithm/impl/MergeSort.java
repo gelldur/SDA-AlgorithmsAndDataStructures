b777b1ea2f565fa5347342771328e983ae66520a
Dawid Drozd <drozddawid.uam@gmail.com>
2017-07-12 14:07:57 +0200

    Add MergeSort empty template

---
+++ algorithms/src/pl/sdacademy/algorithm/impl/MergeSort.java
@@ -0,0 +1,9 @@
+package pl.sdacademy.algorithm.impl;
+
+public class MergeSort implements pl.sdacademy.algorithm.Sort {
+
+	@Override
+	public void sort(int[] array) {
+		// TODO Auto-generated method stub
+	}
+}
