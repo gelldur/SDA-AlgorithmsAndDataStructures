166e7817bf604cf250c8eaab23c88e6e34a29e1c
Dawid Drozd <drozddawid.uam@gmail.com>
2017-07-13 15:11:13 +0200

    Add HanoiTowers

---
+++ data-structures/src/pl/sdacademy/data/structures/HanoiTowers.java
@@ -0,0 +1,37 @@
+package pl.sdacademy.data.structures;
+
+import java.util.Stack;
+
+//@formatter:off
+
+//hanoi(int n, char A, char B, char C)
+//	if (n > 0)
+//		hanoi(n-1, A, C, B);
+//		print "Moving Disk $value from $A to $C"
+//		hanoi(n-1, B, A, C);
+
+// More information: https://en.wikipedia.org/wiki/Tower_of_Hanoi
+//@formatter:on
+
+public class HanoiTowers {
+
+	public HanoiTowers(int countOfDisks) {
+		// TODO implement
+	}
+
+	/**
+	 * @return true if game doesn't end
+	 */
+	public boolean makeStep() {
+		// TODO implement
+		return false;
+	}
+
+	void hanoi(int n, char A, char B, char C) {
+		// TODO implement
+	}
+
+	public Stack<Integer> getStateOfPile(char pileTag) {
+		return null;// TODO implement
+	}
+}
---
+++ data-structures/test/pl/sdacademy/data/structures/HanoiTowerTest.java
@@ -0,0 +1,41 @@
+package pl.sdacademy.data.structures;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HanoiTowerTest {
+
+	@Before
+	public void setUp() throws Exception {
+		_hanoi = new HanoiTowers(4);
+	}
+
+	@Test
+	public void testInitialState() {
+		assertArrayEquals(new Integer[] { 1, 2, 3, 4 }, _hanoi.getStateOfPile('A').toArray());
+		assertArrayEquals(new Integer[] {}, _hanoi.getStateOfPile('B').toArray());
+		assertArrayEquals(new Integer[] {}, _hanoi.getStateOfPile('C').toArray());
+	}
+
+	@Test
+	public void test() {
+		assertArrayEquals(new Integer[] { 1, 2, 3, 4 }, _hanoi.getStateOfPile('A').toArray());
+		_hanoi.makeStep();
+		assertArrayEquals(new Integer[] { 2, 3, 4 }, _hanoi.getStateOfPile('A').toArray());
+		assertArrayEquals(new Integer[] { 1 }, _hanoi.getStateOfPile('B').toArray());
+		_hanoi.makeStep();
+		assertArrayEquals(new Integer[] { 3, 4 }, _hanoi.getStateOfPile('A').toArray());
+		assertArrayEquals(new Integer[] { 1 }, _hanoi.getStateOfPile('B').toArray());
+		assertArrayEquals(new Integer[] { 2 }, _hanoi.getStateOfPile('B').toArray());
+		while(_hanoi.makeStep()) {
+		}
+		
+		assertArrayEquals(new Integer[] {}, _hanoi.getStateOfPile('A').toArray());
+		assertArrayEquals(new Integer[] {}, _hanoi.getStateOfPile('B').toArray());
+		assertArrayEquals(new Integer[] { 1, 2, 3, 4 }, _hanoi.getStateOfPile('C').toArray());
+	}
+
+	private HanoiTowers _hanoi;
+}
