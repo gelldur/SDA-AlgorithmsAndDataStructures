db4d894ef50431ad0105bf38c22473ddb50e9aa1
Dawid Drozd <drozddawid.uam@gmail.com>
2017-09-03 22:48:44 +0200

    Update tests

--- data-structures/test/pl/sdacademy/data/structures/HanoiTowerTest.java
+++ data-structures/test/pl/sdacademy/data/structures/HanoiTowerTest.java
@@ -14,27 +14,27 @@ public class HanoiTowerTest {
 
 	@Test
 	public void testInitialState() {
-		assertArrayEquals(new Integer[] { 1, 2, 3, 4 }, _hanoi.getStateOfPile('A').toArray());
+		assertArrayEquals(new Integer[] { 4, 3, 2, 1 }, _hanoi.getStateOfPile('A').toArray());
 		assertArrayEquals(new Integer[] {}, _hanoi.getStateOfPile('B').toArray());
 		assertArrayEquals(new Integer[] {}, _hanoi.getStateOfPile('C').toArray());
 	}
 
 	@Test
 	public void test() {
-		assertArrayEquals(new Integer[] { 1, 2, 3, 4 }, _hanoi.getStateOfPile('A').toArray());
+		assertArrayEquals(new Integer[] { 4, 3, 2, 1 }, _hanoi.getStateOfPile('A').toArray());
 		_hanoi.makeStep();
-		assertArrayEquals(new Integer[] { 2, 3, 4 }, _hanoi.getStateOfPile('A').toArray());
+		assertArrayEquals(new Integer[] { 4, 3, 2 }, _hanoi.getStateOfPile('A').toArray());
 		assertArrayEquals(new Integer[] { 1 }, _hanoi.getStateOfPile('B').toArray());
 		_hanoi.makeStep();
-		assertArrayEquals(new Integer[] { 3, 4 }, _hanoi.getStateOfPile('A').toArray());
+		assertArrayEquals(new Integer[] { 4, 3 }, _hanoi.getStateOfPile('A').toArray());
 		assertArrayEquals(new Integer[] { 1 }, _hanoi.getStateOfPile('B').toArray());
-		assertArrayEquals(new Integer[] { 2 }, _hanoi.getStateOfPile('B').toArray());
-		while(_hanoi.makeStep()) {
+		assertArrayEquals(new Integer[] { 2 }, _hanoi.getStateOfPile('C').toArray());
+		while (_hanoi.makeStep()) {
 		}
-		
+
 		assertArrayEquals(new Integer[] {}, _hanoi.getStateOfPile('A').toArray());
 		assertArrayEquals(new Integer[] {}, _hanoi.getStateOfPile('B').toArray());
-		assertArrayEquals(new Integer[] { 1, 2, 3, 4 }, _hanoi.getStateOfPile('C').toArray());
+		assertArrayEquals(new Integer[] { 4, 3, 2, 1 }, _hanoi.getStateOfPile('C').toArray());
 	}
 
 	private HanoiTowers _hanoi;
